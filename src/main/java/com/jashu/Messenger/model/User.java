818d45eb035827753f88680640912803263103a1
jaswanth-kota07 <jaswanthkota2007@gmail.com>
2026-01-31 16:47:56 +0530

    changed table and column name in user entity

--- src/main/java/com/jashu/Messenger/model/User.java
+++ src/main/java/com/jashu/Messenger/model/User.java
@@ -4,41 +4,37 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 @Entity
+@Table(name = "users")  // renamed table to avoid 'user' keyword
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String user;
+
+    @Column(name = "username")  // maps DB column 'username'
+    private String user;          // keep Java field 'user'
+
     private String password;
 
-    public int getId() {
-        return id;
-    }
+    // Constructors
+    public User() {}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getUser() {
-        return user;
-    }
-    public void setUser(String user) {
-        this.user = user;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
     public User(String user, String password) {
         this.user = user;
         this.password = password;
     }
 
-    public User() {
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    }
+    public String getUser() { return user; }
+    public void setUser(String user) { this.user = user; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
